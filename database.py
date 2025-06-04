864a0c43cae2850cd92a2aabadfe2b5d4d9e5ddd
yashubeast <95849633+yashubeast@users.noreply.github.com>
2025-06-04 13:22:10 +0530

    fix: minor

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_URL = "sqlite+aiosqlite:///./main.db"
+os.makedirs("./assets", exist_ok=True)
+
+DB_URL = "sqlite+aiosqlite:///./assets/main.db"
 
 engine = create_async_engine(DB_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
